2dd73fe1632621f1fdaeb3e0781468a67c3400d0
Cleiton Dias <s.diascleiton@gmail.com>
2019-05-25 00:12:58 -0300

    Implements the Parcelable in the Class | Implementando o Parcelable na classe

--- app/src/main/java/com/cleitondsd/etec/quizetecv1/Question.java
+++ app/src/main/java/com/cleitondsd/etec/quizetecv1/Question.java
@@ -1,6 +1,9 @@
 package com.cleitondsd.etec.quizetecv1;
 
-public class Question  {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Question implements Parcelable {
 
     private String questao;
     private String opcao1, opcao2, opcao3;
@@ -17,6 +20,40 @@ public class Question  {
     }
 
     //Get e Set
+
+    protected Question(Parcel in) {
+        questao = in.readString();
+        opcao1 = in.readString();
+        opcao2 = in.readString();
+        opcao3 = in.readString();
+        numeroResposta = in.readInt();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(questao);
+        dest.writeString(opcao1);
+        dest.writeString(opcao2);
+        dest.writeString(opcao3);
+        dest.writeInt(numeroResposta);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Question> CREATOR = new Creator<Question>() {
+        @Override
+        public Question createFromParcel(Parcel in) {
+            return new Question(in);
+        }
+
+        @Override
+        public Question[] newArray(int size) {
+            return new Question[size];
+        }
+    };
 
     public String getQuestao() {
         return questao;
