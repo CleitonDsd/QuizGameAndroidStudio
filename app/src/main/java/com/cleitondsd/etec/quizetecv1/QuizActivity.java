dbccb7477c1361d6673321a0f86f0d1a5bcbbe22
Cleiton Dias <s.diascleiton@gmail.com>
2019-06-02 11:52:45 -0300

    Alterando metódo que cria a activity Oncreate utilizando o metodo GetQuestions | Update in method that create the activity and save the instance with the method GetQeustions

--- app/src/main/java/com/cleitondsd/etec/quizetecv1/QuizActivity.java
+++ app/src/main/java/com/cleitondsd/etec/quizetecv1/QuizActivity.java
@@ -79,7 +79,7 @@ public class QuizActivity extends AppCompatActivity {
 
 
             QuizDbHelper dbHelper = new QuizDbHelper(this);
-            questionList = dbHelper.getAllQuestions();
+            questionList = dbHelper.getQuestions("Medium");
             questionCountTotal = questionList.size();
             Collections.shuffle(questionList);
 
