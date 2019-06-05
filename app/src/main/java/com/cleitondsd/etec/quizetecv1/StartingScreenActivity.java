6a930bba32e244e335a980669400cbccd3e5fa1d
Cleiton Dias <s.diascleiton@gmail.com>
2019-06-04 23:32:46 -0300

    Implementando variaveis e metodos responsaveis por organizar e carregar os recursos de categorias e dificulades

--- app/src/main/java/com/cleitondsd/etec/quizetecv1/StartingScreenActivity.java
+++ app/src/main/java/com/cleitondsd/etec/quizetecv1/StartingScreenActivity.java
@@ -13,15 +13,20 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.List;
+
 public class StartingScreenActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_QUIZ = 1;
-    public  static final String EXTRA_DIFFICULTY = "extraDifficulty";
+    public static final String EXTRA_CATEGORY_ID = "extraCategoryID";
+    public static final String EXTRA_CATEGORY_NAME = "extraCategoryName";
+    public static final String EXTRA_DIFFICULTY = "extraDifficulty";
 
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String KEY_HIGHSCORE = "keyHighscore";
 
     private TextView textViewHighscore;
+    private Spinner spinnerCategory;
     private Spinner spinnerDifficulty;
 
     private int highscore;
@@ -32,17 +37,12 @@ public class StartingScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_starting_screen);
 
         textViewHighscore = findViewById(R.id.text_view_highscore);
+        spinnerCategory = findViewById(R.id.spinner_difficulty);
         spinnerDifficulty = findViewById(R.id.spinner_difficulty);
 
 
-        String[] difficultyLevels = Question.getAllDifficultyLevels();
-
-        ArrayAdapter<String> adapterDifficulty = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, difficultyLevels);
-
-        adapterDifficulty.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerDifficulty.setAdapter(adapterDifficulty);
-
+        loadCategories();
+        loadDifficultyLevels();
         loadHighscore();
 
         Button buttonStarQuiz = findViewById(R.id.button_start_quiz);
@@ -57,10 +57,14 @@ public class StartingScreenActivity extends AppCompatActivity {
 
 
     private void startQuiz() {
-
+        Category selectedCategory = (Category) spinnerCategory.getSelectedItem();
+        int categoryID = selectedCategory.getId();
+        String categoryName = selectedCategory.getName();
         String difficulty = spinnerDifficulty.getSelectedItem().toString();
 
         Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        intent.putExtra(EXTRA_CATEGORY_ID, categoryID);
+        intent.putExtra(EXTRA_CATEGORY_NAME, categoryName);
         intent.putExtra(EXTRA_DIFFICULTY, difficulty);
         startActivityForResult(intent, REQUEST_CODE_QUIZ);
     }
@@ -77,6 +81,20 @@ public class StartingScreenActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    private void loadCategories() {
+        QuizDbHelper dbHelper = QuizDbHelper.getInstance(this);
+        List<Category> categories = dbHelper.getAllCategories();
+
+        ArrayAdapter<Category> adapterCategories = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, categories);
+        adapterCategories.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(adapterCategories);
+    }
+
+    private void loadDifficultyLevels() {
+        String[] difficultyLevels = Question.getAllDifficultyLevels();
     }
 
     private void loadHighscore() {
