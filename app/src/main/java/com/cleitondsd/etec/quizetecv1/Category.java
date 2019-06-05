cf253bc902287ca67539582cd523d629776fe184
Cleiton Dias <s.diascleiton@gmail.com>
2019-06-04 22:03:49 -0300

    Classe com a categoria das questões | Class with the category questions

--- app/src/main/java/com/cleitondsd/etec/quizetecv1/Category.java
+++ app/src/main/java/com/cleitondsd/etec/quizetecv1/Category.java
@@ -1,4 +1,34 @@
 package com.cleitondsd.etec.quizetecv1;
 
 public class Category {
+
+    public static final int PROGRAMMING_LOGIC = 1;
+    public static final int JAVA = 2;
+    public static final int SQL = 3;
+
+    private int id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
